8942136ea9547c8ce1bd8d8b57abec2e462ffa90
Martin <martin@kiranatama.com>
2018-09-12 21:03:06 +0700

    fixed sub menu, and brands on home page

--- ang/app/controllers/home_controller.rb
+++ ang/app/controllers/home_controller.rb
@@ -4,6 +4,6 @@ class HomeController < ApplicationController
     @banners = Banner.all.order("`order`, id")
     @best_seller_products = Product.best_sellers.limit(8)
     @promo_products = Product.promos.limit(8)
-    @popular_brands = Brand.popular.limit(6)
+    @popular_brands = Brand.all
   end
 end
