becc85979d2224a537ebc707cefe22f73cc5981e
Martin <martin@kiranatama.com>
2020-08-24 18:31:27 +0700

    fixed captcha

--- ang/config/initializers/recaptcha.rb
+++ ang/config/initializers/recaptcha.rb
@@ -1,5 +1,7 @@
 
 Recaptcha.configure do |config|
-  config.public_key  = '6Lcf6HwUAAAAABUMJuCdT17bepLFSi9iKWzp_qof'
-  config.private_key = '6Lcf6HwUAAAAAEYEpZ8B_ksjW9PoJjA7E70vNoZ0'
+  # config.public_key  = '6Lcf6HwUAAAAABUMJuCdT17bepLFSi9iKWzp_qof'
+  # config.private_key = '6Lcf6HwUAAAAAEYEpZ8B_ksjW9PoJjA7E70vNoZ0'
+  config.public_key  = '6Le3n8IZAAAAACHB-uiXQRqLb_dTCeXmrd4smYPT'
+  config.private_key = '6Le3n8IZAAAAAM36Gmg3TF1Cbzc0KIR29nRk_kVI'
 end
